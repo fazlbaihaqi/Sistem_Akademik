12043e9e2a9fa7276c6128c2fc89fdfc92c48287
fazlbaihaqi <asarmet4lium@gmail.com>
2020-10-18 01:16:15 +0700

    fist commit

---
+++ 1/content_footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs"></div>
+    <strong>Copyright &copy; <?php echo date("Y") ?> Sistem Akademik and Created By <a href="/">fazl Baihaqi Ashar</a></strong>
+</footer>
